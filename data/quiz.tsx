ec2be6f2d43c71e6e3f6e778845fd3f803fe7bc7
Paulo Martins <pauloportelamartins@gmail.com>
2024-06-06 15:04:24 -0300

    Changed type & quiz objects & added Zod

---
+++ data/quiz.tsx
@@ -0,0 +1,95 @@
+const Quizes: QuizTemplate[] = [
+  {
+    id: "",
+    title: "Spooky Scary Questions For a Halloween with @user",
+    description: "Trick or Treat? What costume would @user wear? Find out by answering this spooky quiz!",
+    questionPool: [
+      {
+        id: "",
+        prompt: "You were visited by some kids you don't know. Trick or Treat?",
+        options: [
+          {
+            id: "",
+            prompt: "Trick",
+          },
+          {
+            id: "",
+            prompt: "Treat",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "What costume would @user wear?",
+        options: [
+          {
+            id: "",
+            prompt: "Hot Dog",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Pirate",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Anime",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Cowboy/Cowgirl",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "What hot costume would @user wear?",
+        options: [
+          {
+            id: "",
+            prompt: "Hot Nurse",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Hot Angel",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Hot Witch",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Hot Clown",
+            imageSrc: "",
+          },
+        ],
+      },
+    ],
+    questionAmount: 12,
+    design: {
+      visual: {
+        avatarSrc: "",
+        backdropSrc: "",
+        cardBackgroundSrc: "",
+        mainColor: "",
+        mainShapeSrc: "",
+      },
+      sound: {
+        trackSrc: "",
+        trackFinish: "",
+        sfxGenericClickSrc: "",
+        sfxCorrectSrc: "",
+        sfxIncorrectSrc: "",
+        sfxFinishFanfareSrc: "",
+        sfxFinishFanfareAllCorrectSrc: "",
+        sfxFinishFanfareAllIncorrectSrc: "",
+      },
+    },
+  },
+];
--- types/index.ts
+++ types/index.ts
@@ -2,12 +2,69 @@
 // # Quiz Template -Related
 // ----------------------------------------
 
-type QuizTemplate = {};
+type QuizTemplate = {
+  id: string;
+  title: string;
+  description: string;
+  design: {
+    visual: {
+      avatarSrc: string;
+      backdropSrc: string;
+      cardBackgroundSrc: string;
+      mainColor: string;
+      mainShapeSrc: string;
+    };
+    sound: {
+      trackSrc: string;
+      trackFinish: string;
+      sfxGenericClickSrc: string;
+      sfxCorrectSrc: string;
+      sfxIncorrectSrc: string;
+      sfxFinishFanfareSrc: string;
+      sfxFinishFanfareAllCorrectSrc: string;
+      sfxFinishFanfareAllIncorrectSrc: string;
+    };
+  };
+  questionPool: Question[];
+  questionAmount: number;
+};
 
 type Question = {
+  id: string;
   prompt: string;
-  options: string[];
-  answer: number;
+  options: Option[];
+};
+
+type Option = {
+  id: string;
+  prompt: string;
+  imageSrc?: string;
+};
+
+// ----------------------------------------
+// # Quiz Instance -Related
+// ----------------------------------------
+
+// Make it so everything you need is in here? No, because it saves space in the database, even if costs a few queries more
+// Change question to behave with ID searching
+
+type QuizInstance = {
+  templateID: string;
+  owner: UserProfile;
+  questionList: QuestionAnswer[];
+  leaderboard: QuizAnswerResult[];
+};
+
+// Is used both by the creator and the respondant
+type QuestionAnswer = {
+  questionID: string;
+  answerID: string;
+};
+
+type QuizAnswerResult = {
+  owner: string;
+  questionList: QuestionAnswer[];
+  correctAnswerAmount: number;
 };
 
 // ----------------------------------------
@@ -17,9 +74,3 @@ type UserProfile = {};
 
 type Username = string;
 type Nickname = string;
-
-// ----------------------------------------
-// # Quiz Instance -Related
-// ----------------------------------------
-
-type Result = {};
