9b62f03a33ae4191e7aa4057fd77d598ae78df59
Paulo Martins <pauloportelamartins@gmail.com>
2024-06-13 16:23:51 -0300

    *Added new assets, changed quiz type, started adding Zod schemas
    *Broken build, WIP

--- data/quiz.tsx
+++ data/quiz.tsx
@@ -1,4 +1,4 @@
-const Quizes: QuizTemplate[] = [
+const QuizList: QuizTemplate[] = [
   {
     id: "",
     title: "Spooky Scary Questions For a Halloween with @user",
@@ -70,26 +70,575 @@ const Quizes: QuizTemplate[] = [
           },
         ],
       },
+      {
+        id: "",
+        prompt: "What's @user's favorite Halloween activity?",
+        options: [
+          {
+            id: "",
+            prompt: "Trick-or-treating",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Carving pumpkins",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Watching scary movies",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Attending a costume party",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "Which spooky setting does @user prefer?",
+        options: [
+          {
+            id: "Haunted house",
+            prompt: "",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Creepy forest",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Abandoned asylum",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Graveyard at midnight",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "What's @user's preferred Halloween treat?",
+        options: [
+          {
+            id: "",
+            prompt: "Candy corn",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Pumpkin spice everything",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Chocolate bars",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Caramel apples",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "Which supernatural creature would @user be?",
+        options: [
+          {
+            id: "",
+            prompt: "Vampire",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Ghost",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Werewolf",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Witch",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "How would @user decorate for Halloween?",
+        options: [
+          {
+            id: "",
+            prompt: "Spooky yard decorations",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Indoor haunted house",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Pumpkin everything",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Elegant Gothic style",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "What's @user's ideal Halloween costume?",
+        options: [
+          {
+            id: "",
+            prompt: "Classic monster (like Frankenstein or Dracula)",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Pop culture character (like a superhero or villain)",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Lazy idea (like a blanket ghost)",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Some random obscure thing (like a character from a book nobody read or a concept)",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "Which horror movie genre does @user prefer?",
+        options: [
+          {
+            id: "",
+            prompt: "Slasher films",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Supernatural horror",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Psychological thriller",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Horror-comedy",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "What gives @user the creeps the most?",
+        options: [
+          {
+            id: "",
+            prompt: "Clowns",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Spiders",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "The inescapable concept of mortality",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Dolls",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "How does @user feel about horror stories?",
+        options: [
+          {
+            id: "",
+            prompt: "Love them; I can't get enough!",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "They're too scary for me",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "I'm skeptical but intrigued",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Too silly",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "What's @user's favorite Halloween color scheme?",
+        options: [
+          {
+            id: "",
+            prompt: "Purple and green",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Orange and black",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Red and black",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Pure black",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "What's @user's favorite Halloween drink?",
+        options: [
+          {
+            id: "",
+            prompt: "Pumpkin spice latte",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Hot apple cider",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Blood-red punch",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Never had any",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "Which eery sound does @user find the creepiest?",
+        options: [
+          {
+            id: "",
+            prompt: "Howling wind",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Creaking doors",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Ghostly whispers",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Footsteps in the dark",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "Which Halloween prank would @user most likely play?",
+        options: [
+          {
+            id: "",
+            prompt: "A jump scare",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "A fake spider/cockroach in a surprising place",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Playing dead",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Pretend they saw a ghost",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "What's @user's favorite spooky TV show?",
+        options: [
+          {
+            id: "",
+            prompt: "Stranger Things",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "American Horror Story",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Goosebumps",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Chucky",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "How does @user prefer to spend Halloween night?",
+        options: [
+          {
+            id: "",
+            prompt: "Watching horror movies",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Going to a Halloween party",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Eating candy",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Going to horror-themed events",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "Which eerie location would @user like to explore?",
+        options: [
+          {
+            id: "",
+            prompt: "A haunted house",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "An abandoned amusement park",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "A cursed island",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "A deserted ghost town",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "If @user saw a ghostly figure in their mirror, what would their reaction be?",
+        options: [
+          {
+            id: "",
+            prompt: "Scream and run away",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Freeze and helplessly wait",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Take a picture or video",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Look back and hope nothing's there",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "Which movie does @user find the most unsetling?",
+        options: [
+          {
+            id: "",
+            prompt: "The Exorcist",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Paranormal Activity",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "The Conjuring",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Hereditary",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "Which creepy urban legend would @user least want to be true?",
+        options: [
+          {
+            id: "",
+            prompt: "The Slender Man",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "The Rake",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "The Bunnyman",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "The Black-Eyed Children",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "What supernatural event would @user try to debunk?",
+        options: [
+          {
+            id: "",
+            prompt: "A haunted house",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "A UFO sighting",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "A cryptid encounter",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "A possession case",
+            imageSrc: "",
+          },
+        ],
+      },
+      {
+        id: "",
+        prompt: "If @user heard strange noises in their attic at night, what would they do?",
+        options: [
+          {
+            id: "",
+            prompt: "Investigate immediately",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Call a friend for backup",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Ignore it and hope it stops",
+            imageSrc: "",
+          },
+          {
+            id: "",
+            prompt: "Leave the house and call a professional",
+            imageSrc: "",
+          },
+        ],
+      },
+      //24
     ],
     questionAmount: 12,
     design: {
       visual: {
-        avatarSrc: "",
-        backdropSrc: "",
-        cardBackgroundSrc: "",
-        mainColor: "",
-        mainShapeSrc: "",
+        backdropSrc: "PatternPumpkin",
+        mainColor: "slate",
+        highlightColor: "orange",
       },
       sound: {
-        trackSrc: "",
-        trackFinish: "",
-        sfxGenericClickSrc: "",
-        sfxCorrectSrc: "",
-        sfxIncorrectSrc: "",
-        sfxFinishFanfareSrc: "",
-        sfxFinishFanfareAllCorrectSrc: "",
-        sfxFinishFanfareAllIncorrectSrc: "",
+        trackSrc: ["/assets/songs/Spook -by- PeriTune.mp3", "/assets/songs/Spooky Ride -by- Twin Musicom.mp3"],
       },
     },
   },
 ];
+
+const generateQuiz = (quiz: QuizTemplate): QuizTemplate => {
+  // Generate ID for QuizTemplate
+
+  // Generate ID for questionPool items
+
+  // Generate ID for option items
+
+  // Return statement
+  return quiz;
+};
--- types/index.ts
+++ types/index.ts
@@ -1,32 +1,69 @@
+import { z } from "zod";
+
 // ----------------------------------------
 // # Quiz Template -Related
 // ----------------------------------------
+
+// todo: Use superRefine to treat for questionAmount needing to be less than questionPool.length
+/*
+https://zod.dev/?id=strings
+- For custom messages, see examples in the messages string
+- Remember you can use Zod to make api calls to verify things like if ID exists in backend/db
+  - This requires an refine async and needs to run on a parseAsync()
+
+https://blog.logrocket.com/schema-validation-typescript-zod/
+- See general examples for zod, but don't create a new function to validate. Export the schema and use parse
+
+Use zod.infer to get the types
+
+Start using jsdocs for things. Think of a pattern, just like the hierarchy comment pattern, 
+something easy to follow and use everywhere. Check examples and gpt to learn
+*/
+const quizSchema = z.object({
+  id: z.string().uuid(),
+  title: z.string().min(1).max(200),
+  questionPool: z.string(), //Should be its own schema
+  questionAmount: z.string(),
+  design: z.object({
+    visual: z.object({
+      backdropSrc: z.string(), //Create a pattern schema for Src's and use in all of them
+      mainColor: z.string(),
+      highlightColor: z.string(),
+    }),
+    sound: z.object({
+      trackSrc: z.string(),
+      sfxGenericClickSrc: z.string().optional(),
+      sfxCorrectSrc: z.string().optional(),
+      sfxIncorrectSrc: z.string().optional(),
+      sfxFinishFanfareSrc: z.string().optional(),
+      sfxFinishFanfareAllCorrectSrc: z.string().optional(),
+      sfxFinishFanfareAllIncorrectSrc: z.string().optional(),
+    }),
+  }),
+});
 
 type QuizTemplate = {
   id: string;
   title: string;
   description: string;
-  design: {
-    visual: {
-      avatarSrc: string;
-      backdropSrc: string;
-      cardBackgroundSrc: string;
-      mainColor: string;
-      mainShapeSrc: string;
-    };
-    sound: {
-      trackSrc: string;
-      trackFinish: string;
-      sfxGenericClickSrc: string;
-      sfxCorrectSrc: string;
-      sfxIncorrectSrc: string;
-      sfxFinishFanfareSrc: string;
-      sfxFinishFanfareAllCorrectSrc: string;
-      sfxFinishFanfareAllIncorrectSrc: string;
-    };
-  };
   questionPool: Question[];
   questionAmount: number;
+  design: {
+    visual: {
+      backdropSrc: string;
+      mainColor: string;
+      highlightColor: string;
+    };
+    sound: {
+      trackSrc: string[];
+      sfxGenericClickSrc?: string;
+      sfxCorrectSrc?: string;
+      sfxIncorrectSrc?: string;
+      sfxFinishFanfareSrc?: string;
+      sfxFinishFanfareAllCorrectSrc?: string;
+      sfxFinishFanfareAllIncorrectSrc?: string;
+    };
+  };
 };
 
 type Question = {
