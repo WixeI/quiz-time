28451cf44111da65e608f35330eaa0ce2fc1be06
Paulo Martins <pauloportelamartins@gmail.com>
2024-07-21 00:13:52 -0300

    Typo fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const plugin = require("tailwindcss/plugin");
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/srccomponents/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
